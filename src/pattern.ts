415aca576cc4045cbd07a90783202fd42c993981
刘庆林 <1459307172@qq.com>
2023-12-22 16:55:38 +0800

    fix:合并代码

--- src/pattern.ts
+++ src/pattern.ts
@@ -1,3 +1,7 @@
+/**
+ * 工厂模式
+ * 根据一个上下文类的静态方法，传入不同的参数返回不同的实例
+ */
 namespace FactoryPattern {
   interface Product {
     doSomething(): void;
@@ -27,6 +31,10 @@ namespace FactoryPattern {
   const productA = Factory.createProduct("A");
   productA.doSomething();
 }
+/**
+ * 抽象工厂模式
+ * 直接提供一个实体类的工厂，调用不同的方法返回不同的实例
+ */
 namespace abstractFactoryPattern {
   interface Factory {
     createProductA(): ProductA;
@@ -85,6 +93,9 @@ namespace abstractFactoryPattern {
   productB1.doSomething();
   productB2.doSomething();
 }
+/**
+ * 单例模式
+ */
 namespace SingletonPattern {
   class Singleton {
     private static instance: Singleton;
@@ -99,4 +110,185 @@ namespace SingletonPattern {
   const s1 = Singleton.getInstance(),
     s2 = Singleton.getInstance();
   console.log(s1 === s2);
+}
+/**
+ * 建造者模式
+ * 根据创建者的不同函数构造复杂对象，调用getProduct返回实例并重置实例
+ */
+namespace BuilderPattern {
+  class Product {
+    public parts: Array<string> = [];
+    public listParts() {
+      console.log(`Product parts: ${this.parts.join(", ")}\n`);
+    }
+  }
+  class Builder {
+    product: Product;
+    public reset() {
+      this.product = new Product();
+    }
+    constructor() {
+      this.product = new Product();
+    }
+    buildPartA() {
+      this.product.parts.push("PartA");
+    }
+    buildPartB() {
+      this.product.parts.push("PartB");
+    }
+    getProduct(): Product {
+      const result = this.product;
+      this.reset();
+      return result;
+    }
+  }
+  const b = new Builder();
+  b.buildPartA();
+  b.buildPartB();
+  const product = b.getProduct();
+  product.listParts();
+  const product2 = b.getProduct();
+  product2.listParts();
+}
+/**
+ * 策略模式
+ */
+namespace StrategyPattern {
+  class MemberShip {
+    calculateDiscout(price: number) {
+      throw new Error("Not implemented yet");
+    }
+  }
+  class BronzeMemberShip extends MemberShip {
+    calculateDiscout(price: number) {
+      return price * 0.1;
+    }
+  }
+  class SilverMemberShip extends MemberShip {
+    calculateDiscout(price: number) {
+      return price * 0.2;
+    }
+  }
+  class GoldMemberShip extends MemberShip {
+    calculateDiscout(price: number) {
+      return price * 0.5;
+    }
+  }
+  class discoutContext {
+    strategy: MemberShip;
+    constructor(memberShip: string) {
+      switch (memberShip) {
+        case "bronze":
+          this.strategy = new BronzeMemberShip();
+          break;
+        case "silver":
+          this.strategy = new SilverMemberShip();
+          break;
+        case "gold":
+          this.strategy = new GoldMemberShip();
+          break;
+        default:
+          this.strategy = new MemberShip();
+      }
+    }
+    calculateDiscout(price: number) {
+      return this.strategy.calculateDiscout(price);
+    }
+  }
+  console.log(new discoutContext("bronze").calculateDiscout(1000));
+  console.log(new discoutContext("silver").calculateDiscout(1000));
+  console.log(new discoutContext("gold").calculateDiscout(1000));
+  /**
+   * 策略模式 函数实现
+   */
+  export namespace FunctionStrategy {
+    const strategy = {
+      bike: () => {
+        console.log("bike");
+      },
+      electromobile: () => {
+        console.log("electromobile");
+      },
+      car: () => {
+        console.log("car");
+      },
+    };
+    const travel = (way: "bike" | "electromobile" | "car") => {
+      const strategyFunc = strategy[way];
+      if (!strategyFunc) {
+        throw new Error("way is not found");
+      }
+      return strategyFunc();
+    };
+    travel("bike");
+    travel("electromobile");
+    travel("car");
+  }
+}
+/**
+ * 发布订阅模式 利用中介（消息代理与或事件通道）与订阅者耦合性较低
+ */
+namespace SubScribeStrategy {
+  interface subscribe {
+    update: (data: string) => void;
+  }
+  class ConcreteSubscribe implements subscribe {
+    update(data: any): void {
+      console.log(`update - ${data}`);
+    }
+  }
+  class publish {
+    private subscribes: { [event: string]: subscribe[] } = {};
+    subscribe(event: string, subscribe: subscribe) {
+      !this.subscribes[event] && (this.subscribes[event] = []);
+      this.subscribes[event].push(subscribe);
+    }
+    publish(event: string, data: any) {
+      const subscribers = this.subscribes[event];
+      subscribers &&
+        subscribers.forEach((subscribe) => {
+          subscribe.update(data);
+        });
+    }
+  }
+  const pub = new publish();
+  const sub = new ConcreteSubscribe();
+  const sub2 = new ConcreteSubscribe();
+  pub.subscribe("event", sub);
+  pub.subscribe("event", sub2);
+  pub.publish("event", "hello world");
+}
+/**
+ * 观察者模式
+ */
+namespace ObserverPattern {
+  interface Observer {
+    update: (data: string) => void;
+  }
+
+  class ConcreteObserver implements Observer {
+    componentName: string;
+    update(message: string): void {
+      console.log(`${this.componentName} - ${message}`);
+    }
+    constructor(s?: string) {
+      this.componentName = s ?? "";
+    }
+  }
+  class Subject {
+    private observers: Observer[] = [];
+    addObserver(observer: Observer) {
+      this.observers.push(observer);
+    }
+    notify(message: string) {
+      this.observers.forEach((observer) => {
+        observer.update(message);
+      });
+    }
+  }
+  let subject = new Subject();
+  let observer1 = new ConcreteObserver("app");
+  let observer2 = new ConcreteObserver("");
+  subject.addObserver(observer1);
+  subject.notify("hello world");
 }
--- src/test/pattern.js
+++ src/test/pattern.js
@@ -1,4 +1,23 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+/**
+ * 工厂模式
+ * 根据一个上下文类的静态方法，传入不同的参数返回不同的实例
+ */
 var FactoryPattern;
 (function (FactoryPattern) {
     var ConcreteProductA = /** @class */ (function () {
@@ -35,6 +54,10 @@ var FactoryPattern;
     var productA = Factory.createProduct("A");
     productA.doSomething();
 })(FactoryPattern || (FactoryPattern = {}));
+/**
+ * 抽象工厂模式
+ * 直接提供一个实体类的工厂，调用不同的方法返回不同的实例
+ */
 var abstractFactoryPattern;
 (function (abstractFactoryPattern) {
     var ConcreteFactory1 = /** @class */ (function () {
@@ -102,6 +125,9 @@ var abstractFactoryPattern;
     productB1.doSomething();
     productB2.doSomething();
 })(abstractFactoryPattern || (abstractFactoryPattern = {}));
+/**
+ * 单例模式
+ */
 var SingletonPattern;
 (function (SingletonPattern) {
     var Singleton = /** @class */ (function () {
@@ -118,3 +144,212 @@ var SingletonPattern;
     var s1 = Singleton.getInstance(), s2 = Singleton.getInstance();
     console.log(s1 === s2);
 })(SingletonPattern || (SingletonPattern = {}));
+/**
+ * 建造者模式
+ * 根据创建者的不同函数构造复杂对象，调用getProduct返回实例并重置实例
+ */
+var BuilderPattern;
+(function (BuilderPattern) {
+    var Product = /** @class */ (function () {
+        function Product() {
+            this.parts = [];
+        }
+        Product.prototype.listParts = function () {
+            console.log("Product parts: ".concat(this.parts.join(", "), "\n"));
+        };
+        return Product;
+    }());
+    var Builder = /** @class */ (function () {
+        function Builder() {
+            this.product = new Product();
+        }
+        Builder.prototype.reset = function () {
+            this.product = new Product();
+        };
+        Builder.prototype.buildPartA = function () {
+            this.product.parts.push("PartA");
+        };
+        Builder.prototype.buildPartB = function () {
+            this.product.parts.push("PartB");
+        };
+        Builder.prototype.getProduct = function () {
+            var result = this.product;
+            this.reset();
+            return result;
+        };
+        return Builder;
+    }());
+    var b = new Builder();
+    b.buildPartA();
+    b.buildPartB();
+    var product = b.getProduct();
+    product.listParts();
+    var product2 = b.getProduct();
+    product2.listParts();
+})(BuilderPattern || (BuilderPattern = {}));
+/**
+ * 策略模式
+ */
+var StrategyPattern;
+(function (StrategyPattern) {
+    var MemberShip = /** @class */ (function () {
+        function MemberShip() {
+        }
+        MemberShip.prototype.calculateDiscout = function (price) {
+            throw new Error("Not implemented yet");
+        };
+        return MemberShip;
+    }());
+    var BronzeMemberShip = /** @class */ (function (_super) {
+        __extends(BronzeMemberShip, _super);
+        function BronzeMemberShip() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        BronzeMemberShip.prototype.calculateDiscout = function (price) {
+            return price * 0.1;
+        };
+        return BronzeMemberShip;
+    }(MemberShip));
+    var SilverMemberShip = /** @class */ (function (_super) {
+        __extends(SilverMemberShip, _super);
+        function SilverMemberShip() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        SilverMemberShip.prototype.calculateDiscout = function (price) {
+            return price * 0.2;
+        };
+        return SilverMemberShip;
+    }(MemberShip));
+    var GoldMemberShip = /** @class */ (function (_super) {
+        __extends(GoldMemberShip, _super);
+        function GoldMemberShip() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        GoldMemberShip.prototype.calculateDiscout = function (price) {
+            return price * 0.5;
+        };
+        return GoldMemberShip;
+    }(MemberShip));
+    var discoutContext = /** @class */ (function () {
+        function discoutContext(memberShip) {
+            switch (memberShip) {
+                case "bronze":
+                    this.strategy = new BronzeMemberShip();
+                    break;
+                case "silver":
+                    this.strategy = new SilverMemberShip();
+                    break;
+                case "gold":
+                    this.strategy = new GoldMemberShip();
+                    break;
+                default:
+                    this.strategy = new MemberShip();
+            }
+        }
+        discoutContext.prototype.calculateDiscout = function (price) {
+            return this.strategy.calculateDiscout(price);
+        };
+        return discoutContext;
+    }());
+    console.log(new discoutContext("bronze").calculateDiscout(1000));
+    console.log(new discoutContext("silver").calculateDiscout(1000));
+    console.log(new discoutContext("gold").calculateDiscout(1000));
+    /**
+     * 策略模式 函数实现
+     */
+    var FunctionStrategy;
+    (function (FunctionStrategy) {
+        var strategy = {
+            bike: function () {
+                console.log("bike");
+            },
+            electromobile: function () {
+                console.log("electromobile");
+            },
+            car: function () {
+                console.log("car");
+            },
+        };
+        var travel = function (way) {
+            var strategyFunc = strategy[way];
+            if (!strategyFunc) {
+                throw new Error("way is not found");
+            }
+            return strategyFunc();
+        };
+        travel("bike");
+        travel("electromobile");
+        travel("car");
+    })(FunctionStrategy = StrategyPattern.FunctionStrategy || (StrategyPattern.FunctionStrategy = {}));
+})(StrategyPattern || (StrategyPattern = {}));
+/**
+ * 发布订阅模式 利用中介（消息代理与或事件通道）与订阅者耦合性较低
+ */
+var SubScribeStrategy;
+(function (SubScribeStrategy) {
+    var ConcreteSubscribe = /** @class */ (function () {
+        function ConcreteSubscribe() {
+        }
+        ConcreteSubscribe.prototype.update = function (data) {
+            console.log("update - ".concat(data));
+        };
+        return ConcreteSubscribe;
+    }());
+    var publish = /** @class */ (function () {
+        function publish() {
+            this.subscribes = {};
+        }
+        publish.prototype.subscribe = function (event, subscribe) {
+            !this.subscribes[event] && (this.subscribes[event] = []);
+            this.subscribes[event].push(subscribe);
+        };
+        publish.prototype.publish = function (event, data) {
+            var subscribers = this.subscribes[event];
+            subscribers &&
+                subscribers.forEach(function (subscribe) {
+                    subscribe.update(data);
+                });
+        };
+        return publish;
+    }());
+    var pub = new publish();
+    var sub = new ConcreteSubscribe();
+    var sub2 = new ConcreteSubscribe();
+    pub.subscribe("event", sub);
+    pub.subscribe("event", sub2);
+    pub.publish("event", "hello world");
+})(SubScribeStrategy || (SubScribeStrategy = {}));
+/**
+ * 观察者模式
+ */
+var ObserverPattern;
+(function (ObserverPattern) {
+    var ConcreteObserver = /** @class */ (function () {
+        function ConcreteObserver(s) {
+            this.componentName = s !== null && s !== void 0 ? s : "";
+        }
+        ConcreteObserver.prototype.update = function (message) {
+            console.log("".concat(this.componentName, " - ").concat(message));
+        };
+        return ConcreteObserver;
+    }());
+    var Subject = /** @class */ (function () {
+        function Subject() {
+            this.observers = [];
+        }
+        Subject.prototype.addObserver = function (observer) {
+            this.observers.push(observer);
+        };
+        Subject.prototype.notify = function (message) {
+            this.observers.forEach(function (observer) {
+                observer.update(message);
+            });
+        };
+        return Subject;
+    }());
+    var subject = new Subject();
+    var observer1 = new ConcreteObserver("app");
+    var observer2 = new ConcreteObserver("");
+    subject.addObserver(observer1);
+    subject.notify("hello world");
+})(ObserverPattern || (ObserverPattern = {}));
